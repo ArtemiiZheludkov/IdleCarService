43773c1eecd31694ec06336a665c77224078fa28
Artemii Zheludkov <artemii.zheludkov@gmail.com>
2025-03-08 18:04:35 +0200

    add craft system

---
+++ Assets/Scripts/UI/GamePlay/Craft/CraftItemView.cs
@@ -0,0 +1,79 @@
+﻿using IdleCarService.Craft;
+using IdleCarService.Inventory;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IdleCarService.UI.GamePlay
+{
+    public class CraftItemView : MonoBehaviour
+    {
+        [SerializeField] private Image _mainIcon;
+        [SerializeField] private Button _craftButton;
+        
+        [SerializeField] private Image _ingredientPrefab;
+        [SerializeField] private Transform _ingredientsParent;
+        
+        private ItemConfig _config;
+        private CraftManager _crafter;
+        private InventoryManager _inventory;
+
+        private void OnEnable()
+        {
+            UpdateCraftButton();
+            
+            _craftButton.onClick.AddListener(OnCraftClicked);
+            
+            _inventory.OnItemQuantityChanged += OnItemQuantityChanged;
+        }
+
+        private void OnDisable()
+        {
+            UpdateCraftButton();
+            
+            _craftButton.onClick.RemoveListener(OnCraftClicked);
+            
+            _inventory.OnItemQuantityChanged -= OnItemQuantityChanged;
+        }
+
+        public void Init(ItemConfig config, CraftManager crafter, InventoryManager inventory)
+        {
+            _config = config;
+            _crafter = crafter;
+            _inventory = inventory;
+            
+            _mainIcon.sprite = config.Icon;
+            
+            UpdateCraftButton();
+            CreateViews(config);
+        }
+        
+        private void CreateViews(ItemConfig config)
+        {
+            foreach (ItemConfig ingredient in config.CraftingIngredients)
+            {
+                Image icon = Instantiate(_ingredientPrefab, _ingredientsParent);
+                icon.sprite = ingredient.Icon;
+            }
+        }
+
+        private void UpdateCraftButton() => _craftButton.interactable = _crafter != null && _crafter.CanCraftItem(_config.Id);
+
+        private void OnItemQuantityChanged(int id, int count)
+        {
+            foreach (ItemConfig ingredient in _config.CraftingIngredients)
+            {
+                if (ingredient.Id == id)
+                {
+                    UpdateCraftButton();
+                    return;
+                }
+            }
+        }
+
+        private void OnCraftClicked()
+        {
+            _crafter?.TryCraftItem(_config.Id);
+            UpdateCraftButton();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GamePlay/Craft/CraftView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using IdleCarService.Craft;
+using IdleCarService.Inventory;
+using IdleCarService.Progression;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IdleCarService.UI.GamePlay
+{
+    public class CraftView : MonoBehaviour
+    {
+        [SerializeField] private CraftItemView _prefabItemView;
+        [SerializeField] private Transform _contentParent;
+        [SerializeField] private Button _closeButton;
+        
+        private CraftManager _craft;
+        private InventoryManager _inventory;
+        
+        private Dictionary<int, CraftItemView> _itemViews;
+        
+        public void Init(CraftManager craftManager, InventoryManager inventoryManager, LevelController levelController)
+        {
+            _craft = craftManager;
+            _inventory = inventoryManager;
+            
+            CreateViews();
+            
+            levelController.LevelChanged += OnLevelChanged;
+        }
+
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+            _closeButton.onClick.AddListener(Disable);
+        }
+
+        public void Disable()
+        {
+            gameObject.SetActive(false);
+            _closeButton.onClick.RemoveListener(Disable);
+        }
+
+        private void CreateViews()
+        {
+            if (_itemViews == null)
+                _itemViews = new Dictionary<int, CraftItemView>();
+
+            List<ItemConfig> unlockedConfigs = _craft.GetUnlockedCraftableItems();
+
+            foreach (ItemConfig config in unlockedConfigs)
+            {
+                if (_itemViews.ContainsKey(config.Id) == false)
+                    CreateItemView(config);
+            }
+        }
+
+        private void CreateItemView(ItemConfig config)
+        {
+            CraftItemView itemView = Instantiate(_prefabItemView, _contentParent);
+            itemView.Init(config, _craft, _inventory);
+            _itemViews.Add(config.Id, itemView);
+        }
+
+        private void OnLevelChanged(int level) => CreateViews();
+    }
+}
